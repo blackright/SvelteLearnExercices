c234c17732597f5c6c2310941d88fe848288756d
Jason okoka <18481960+blackright@users.noreply.github.com>
2024-01-26 21:44:57 +0300

    Reactive statement in Svelte

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,14 +1,22 @@
 <script>
   let count = 0;
+  let count2 = 0
+  let string;
+
+  function setCount2(x) {
+    count2 = x;
+  }
+
   function increament() {
     count++
   }
+
+  $: setCount2(count)
+  $: string = `The Variable Count2 is ${count2}`
 </script>
 
-<button on:click={increament}>
-  Click {count}
-</button>
-
+<button on:click={increament}>Click {count}</button>
+<h3>{string}</h3>
 
 <style>
   button {
