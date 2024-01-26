a3d3146da53e479418562da7f2be9939d8f728ae
Jason okoka <18481960+blackright@users.noreply.github.com>
2024-01-26 18:06:00 +0300

    Create a stating a state in Svelte

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,19 @@
 <script>
-  let count = 0
-  const increment = () => {
-    count += 1
+  let count = 0;
+  function increament() {
+    count++
   }
 </script>
 
-<button on:click={increment}>
-  count is {count}
+<button on:click={increament}>
+  Click {count}
 </button>
+
+
+<style>
+  button {
+    padding: 15px 20px 15px 20px;
+    border-radius: 10px;
+    border-color: blue;
+  }
+</style>
