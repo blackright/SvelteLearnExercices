576fc6d9fbeb613637eb87fa0a1f2aeb99596902
Jason okoka <18481960+blackright@users.noreply.github.com>
2024-01-26 23:46:26 +0300

    Class directive in Svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,21 @@
 <script>
   import Counter from "./lib/Counter.svelte";
+  import Button from "./lib/Button.svelte";
+
+  let name = "Svelte"
 </script>
 
 <Counter />
+<Button size='large' shadow>Something interesting</Button>
+
+<h1>You are welcome to the 
+  <u>
+    <strong>{name}</strong>
+  </u> Tutorial.
+</h1>
 
 <style>
-
+ strong {
+  color: rgb(244, 47, 3)
+ }
 </style>
---
+++ src/lib/Button.svelte
@@ -0,0 +1,36 @@
+<script>
+    //export let title = "This is My Button Prop";
+    export let size = 'small';
+    export let shadow = false;
+</script>
+
+<button 
+    class={size == 'large' ? 'size-lg' : 'size:sm'}
+    class:shadow><slot />
+</button>
+
+<style lang="scss">
+    button {
+        border-radius: 10px;
+        border-color: brown;
+        color:blanchedalmond;
+        cursor: pointer;
+        &:hover {
+            background-color: black;
+            background-image: linear-gradient(rgba(0,0,0,0.4) 0 0);
+        }
+        &:active {
+            background-image: linear-gradient(rgba(255,255,255,0.1) 0 0);
+        }
+        &.size-sm {
+            padding: 20px 25px 20px 25px;
+        }
+        &.size-lg {
+            padding: 25px 30px 25px 30px;
+            font-size: 25px;
+        }
+        &.shadow {
+            box-shadow: 0 0 20px;
+        }
+    }
+</style>
